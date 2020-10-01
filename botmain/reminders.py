5b7dbccc77b98834f89dc9347b3600ad9e6f0341
michaeldchin <mchin461@gmail.com>
2020-09-30 21:01:50 -0500

    Fixing daylight savings issue

--- botmain/reminders.py
+++ botmain/reminders.py
@@ -1,11 +1,13 @@
 import re
 import dateparser
-import arrow
 from datetime import datetime
 from botmain.utils import clean_everyhere
 from botmain.dbsetup import insert_reminder, query_reminders, query_user_reminders, clear_user_reminders
 import time
 import asyncio
+
+_DATE_FORMAT = '%m/%d/%Y, %I:%M:%S%p %Z'
+_SLEEP_INTERVAL_SECONDS = 8
 
 
 async def send_reminders(bot, data):
@@ -24,7 +26,7 @@ async def check_reminders(bot):
     while True:
         res = query_reminders()
         await send_reminders(bot, res)
-        await asyncio.sleep(8)
+        await asyncio.sleep(_SLEEP_INTERVAL_SECONDS)
 
 
 def add_reminder(ctx):
@@ -36,7 +38,7 @@ def process_reminder(ctx, reminder, time_string):
     date = _get_reminder_date(time_string)
     timestamp = date.timestamp()
     if timestamp < time.time():
-        return f"You're too late. Provided time {date.strftime('%m/%d/%Y, %I:%M%p %Z')} has already passed."
+        return f"You're too late. Provided time {date.strftime(_DATE_FORMAT)} has already passed."
     else:
         user_id = ctx.author.id
         clean_reminder = clean_everyhere(reminder)
@@ -45,7 +47,7 @@ def process_reminder(ctx, reminder, time_string):
                         ctx.channel.id,
                         clean_reminder)
 
-        response = f"You will be reminded '{clean_reminder}' at {date.strftime('%m/%d/%Y, %I:%M%p %Z')}"
+        response = f"You will be reminded '{clean_reminder}' at {date.strftime(_DATE_FORMAT)}"
         return response
 
 
@@ -58,7 +60,7 @@ def show_user_reminders(ctx):
     for datum in data:
         timestamp = datum[0]
         message = datum[1]
-        datestring = datetime.fromtimestamp(timestamp).strftime('%m/%d/%Y, %I:%M%p %Z')
+        datestring = datetime.fromtimestamp(timestamp).strftime(_DATE_FORMAT)
         output += f"{message} at {datestring}\n"
     return output
 
@@ -89,20 +91,5 @@ def _is_utc(date):
 
 
 def _get_reminder_date(timestring):
-    # TODO: Currently returns CST-6 always. Want DST compatibility
-    return dateparser.parse(timestring, settings={'TIMEZONE': 'CST', 'RETURN_AS_TIMEZONE_AWARE': True})
+    return dateparser.parse(timestring, settings={'TIMEZONE': 'US/Central', 'RETURN_AS_TIMEZONE_AWARE': True})
 
-
-def _debug_get_time():
-    testtime = dateparser.parse('10/20 at 5pm')
-    print(testtime)
-    print(arrow.get(testtime))
-    a = arrow.get(testtime).timestamp
-    b = arrow.now('US/Central').timestamp
-    c = time.time()
-    print(a)
-    # print(b)
-    # print(c)
-
-
-_debug_get_time()
