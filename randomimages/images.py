5bdd3cdf159c3b062d10a620098376de10d291d3
michaeldchin <mchin461@gmail.com>
2022-03-10 23:32:42 -0600

    fixing shounen image lower() on None

--- randomimages/images.py
+++ randomimages/images.py
@@ -9,13 +9,13 @@ def _format_quote(quote_list):
 
 
 def handle_image(ctx, quote_id, image_id):
-    if quote_id.lower() == 'random':
+    if quote_id is not None and quote_id.lower() == 'random':
         quoteData = dbsetup.get_quote(guild_id=ctx.guild.id)
     else:
         quoteData = dbsetup.get_quote(quote_id, guild_id=ctx.guild.id)
     quote = _format_quote(quoteData)
 
-    if image_id.lower() == 'custom':
+    if image_id is not None and image_id.lower() == 'custom':
         image_result = dbsetup.get_guild_image(guild_id=ctx.guild.id)
     else:
         image_result = dbsetup.get_image(image_id, guild_id=ctx.guild.id)
